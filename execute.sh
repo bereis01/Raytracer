44c1ff5216f4755b137f6e6b02d02cd80557d0af
Bernardo Reis de Almeida <bernardo.reis.almeida01@gmail.com>
2025-12-08 20:10:28 -0300

    Organized commands

--- execute.sh
+++
@@ -1,6 +0,0 @@
-./build/raytracer test1.in test1.ppm
-./build/raytracer test2.in test2.ppm
-./build/raytracer test3.in test3.ppm
-./build/raytracer test4.in test4.ppm
-./build/raytracer test5.in test5.ppm
-./build/raytracer test6.in test6.ppm
